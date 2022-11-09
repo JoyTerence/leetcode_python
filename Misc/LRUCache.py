bbfb558ba265f4f750375edc83a58c0a63b4647f
Joy Terence <joyterencebarnes@gmail.com>
2022-11-08 19:32:01 -0800

    Update LRUCache.py

--- Misc/LRUCache.py
+++ Misc/LRUCache.py
@@ -1,69 +1,100 @@
-class Node:
-    def __init__(self, key="", val=""):
-        self.key = key
-        self.val = val
-        self.right = None
-        self.left = None
+class DLinkedNode(): 
+    def __init__(self):
+        self.key = 0
+        self.value = 0
+        self.prev = None
+        self.next = None
+            
+class LRUCache():
+    def _add_node(self, node):
+        """
+        Always add the new node right after head.
+        """
+        node.prev = self.head
+        node.next = self.head.next
 
-class LRUCache:
+        self.head.next.prev = node
+        self.head.next = node
 
-    def __init__(self, capacity: int):
+    def _remove_node(self, node):
+        """
+        Remove an existing node from the linked list.
+        """
+        prev = node.prev
+        new = node.next
+
+        prev.next = new
+        new.prev = prev
+
+    def _move_to_head(self, node):
+        """
+        Move certain node in between to the head.
+        """
+        self._remove_node(node)
+        self._add_node(node)
+
+    def _pop_tail(self):
+        """
+        Pop the current tail.
+        """
+        res = self.tail.prev
+        self._remove_node(res)
+        return res
+
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
         self.cache = {}
-        self.head = Node()
-        self.tail = Node()
-        self.head.right = self.tail
-        self.tail.left = self.head
-        self.size = -1
+        self.size = 0
         self.capacity = capacity
+        self.head, self.tail = DLinkedNode(), DLinkedNode()
 
-    def get(self, key: int) -> int:
-        node = self.cache.get(key, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
         
-        if node == -1: return -1
-        
-        self.remove_node(node)
-        self.add_to_front(node)
-        return node.val
-            
-        
-    def put(self, key: int, value: int) -> None:
-        self.size += 1
-        if self.size < self.capacity:
-            node = Node(key, value)
-            self.add_to_front(node)
-            self.cache[key] = node
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        node = self.cache.get(key, None)
+        if not node:
+            return -1
+
+        # move the accessed node to the head;
+        self._move_to_head(node)
+
+        return node.value
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: void
+        """
+        node = self.cache.get(key)
+
+        if not node: 
+            newNode = DLinkedNode()
+            newNode.key = key
+            newNode.value = value
+
+            self.cache[key] = newNode
+            self._add_node(newNode)
+
+            self.size += 1
+
+            if self.size > self.capacity:
+                # pop the tail
+                tail = self._pop_tail()
+                del self.cache[tail.key]
+                self.size -= 1
         else:
-            node = self.cache.get(key, None)
-            
-            if node:
-                node.val = value
-                if node.key in self.cache:
-                    self.cache.pop(node.key)
-                self.remove_node(node)
-                self.add_to_front(node)
-                self.cache[key] = node
-            else:
-                node = self.tail.left
-                if node.key in self.cache:
-                    self.cache.pop(node.key)
-                node.val = value
-                node.key = key
-                self.remove_node(node)
-                self.add_to_front(node)
-                self.cache[key] = node
-                    
-        temp = self.head
-    
-    def add_to_front(self, node):
-        node.left = self.head
-        node.right = self.head.right
-        
-        self.head.right.left = node
-        self.head.right = node
-        
-    def remove_node(self, node):
-        node.left.right = node.right
-        node.right.left = node.left
+            # update the value.
+            node.value = value
+            self._move_to_head(node)
 
 '''
 Time complexity : O(1) both for put and get.
